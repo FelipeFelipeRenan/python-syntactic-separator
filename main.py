db2e10a4588658c6ef23b97a45416593e405ba74
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-06 14:15:32 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import re
+
+file = "teste.py"
+linhas = []
+aux = 0
+
+with open(file, "r") as f:
+    linhas = f.readlines()
+    number = 0
+    for linha in linhas:
+        number = len(re.findall("[  ] ", linha))
+        n =  len(re.findall("[ ]{3}[^aA-zA]|[aA-Zz]\n", linha))
+        print(re.sub("[ ]{2,1000}[^aA-zA]|[aA-Zz]\n", "#####" * n, linha ))
+        print(len(re.findall("[ ]{3}[^aA-zA]|[aA-Zz]\n", linha)))
+        
+        aux += 1
+    print(aux)  
+    print(number)
+    
+
+
+
+#[ ]{2,1000}[^aA-zA]|[aA-Zz]\n
