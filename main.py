465a93456aa79c1f58a0ceac1bf20d3b1bf0a013
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-18 15:42:48 -0300

    Adding a proper output file

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ import os
 
 file = "teste.py"
 linhas = []
+linhas2 = []
+
 aux = 0
 n = 0
 
-fileAux = open("out.txt", "a+")
-
-
-with open(file, "r") as f:
+with open(file, "r") as f, open("out.txt", "a+") as fAux:
     linhas = f.readlines()
+    linhas2 = fAux.readlines()
 
     for linha in linhas:
         aux = n
         number = len(re.findall("[  ] ", linha))
         n =  len(re.findall("[ ]{3}[^aA-zA]|[aA-Zz]\n", linha))
         if n > aux:
-            print(re.sub("[ ]{2,1000}[^aA-zA]|[aA-Zz]\n", "#####" * n, linha ), aux)
+            linhaMaior = re.sub("[ ]{2,1000}[^aA-zA]|[aA-Zz]\n", "#####" * n, linha )
+            fAux.write(f"{linhaMaior}")
+            continue
         #print(len(re.findall("[ ]{3}[^aA-zA]|[aA-Zz]\n", linha)))
-        
+        fAux.write(f"{linha}")
 
 #[ ]{2,1000}[^aA-zA]|[aA-Zz]\n
