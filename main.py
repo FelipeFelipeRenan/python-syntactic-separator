2cb3ef6a638e3ab5b3538212eb3081a3d3571b16
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-01-18 15:16:12 -0300

    Adding tab conditionals

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import re
+import os
 
 file = "teste.py"
 linhas = []
 aux = 0
+n = 0
+
+fileAux = open("out.txt", "a+")
+
 
 with open(file, "r") as f:
     linhas = f.readlines()
-    number = 0
+
     for linha in linhas:
+        aux = n
         number = len(re.findall("[  ] ", linha))
         n =  len(re.findall("[ ]{3}[^aA-zA]|[aA-Zz]\n", linha))
-        print(re.sub("[ ]{2,1000}[^aA-zA]|[aA-Zz]\n", "#####" * n, linha ))
-        print(len(re.findall("[ ]{3}[^aA-zA]|[aA-Zz]\n", linha)))
+        if n > aux:
+            print(re.sub("[ ]{2,1000}[^aA-zA]|[aA-Zz]\n", "#####" * n, linha ), aux)
+        #print(len(re.findall("[ ]{3}[^aA-zA]|[aA-Zz]\n", linha)))
         
-        aux += 1
-
-
 
 #[ ]{2,1000}[^aA-zA]|[aA-Zz]\n
