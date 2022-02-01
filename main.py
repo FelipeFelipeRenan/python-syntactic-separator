cc252f3601a0b80322033db1317c057e83731479
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-01 16:35:04 -0300

    Fixing the regex and adding the search method from re module

--- main.py
+++ main.py
@@ -10,11 +10,18 @@ n = 0
 with open(file, "r") as f, open("out.txt", "w+") as fAux:
     linhas = f.readlines()
     linhas2 = fAux.readlines()
+    
 
     for linha in linhas:
         aux = n
         number = len(re.findall("[  ] ", linha))
         n =  len(re.findall("[ ]{3}[^aA-zA]|[aA-Zz]\n", linha))
+        
+        if(re.search("\)", linha)):
+            linhasAux = re.sub("\)", "\n####\n)", linha)
+        
+            fAux.write(f"{linhasAux}")
+            continue
         if n >= aux:
 
             linhaMaior = re.sub("[ ]{2,1000}[^aA-zZ]|\([aA-zZ]\)\n", "####\n" * n, linha )
@@ -22,6 +29,18 @@ with open(file, "r") as f, open("out.txt", "w+") as fAux:
             fAux.write(f"{linhaMaior}")
             continue
         fAux.write(f"{linha}")
+
+        
+"""
+
+        """
+
+        
+'''
+    for linha in linhas2:
+        
+        
+        fAux.write(f"{linhasAux}") 
     
     for linha in linhas2:
         
@@ -32,6 +51,6 @@ with open(file, "r") as f, open("out.txt", "w+") as fAux:
         
         linhasAux = re.sub("[aA-zZ]{0,100}\)", "####\n", linha)
         fAux.write(f"{linhasAux}")
-
+'''
 #[ ]{2,1000}[^aA-zA]|[aA-Zz]\n
 #[ ]{2,1000}[^aA-zZ]|[$aA-zZ]:
