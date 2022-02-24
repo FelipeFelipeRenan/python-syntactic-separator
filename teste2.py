bbd46d77d525d0b313a8d8341f750d7b40d7c57f
FelipeRenanRenan <feliperenanqwerty@gmail.com>
2022-02-24 13:16:23 -0300

    Adding tests

---
+++ teste2.py
@@ -0,0 +1,21 @@
+def primo(x):
+    counter = 1
+    flag = 0
+    while counter <= x:
+        if x%counter == 0:
+            flag += 1
+            counter +=1
+            continue
+        counter += 1
+    if flag <= 2:
+        return 1
+    else:
+        return 0
+
+if __name__ == "__main__":
+    n = int(input("Digite o numero: "))
+    if primo(n):
+        print("Primo")
+    else:
+        print("Não primo")
+
---
+++ teste3.py
@@ -0,0 +1,21 @@
+def primo(num) -> int:
+    counter = 1
+    flag = 0
+    while counter <= num:
+        if num%counter == 0:
+            flag += 1
+            counter +=1
+            continue
+        counter += 1
+    if flag <= 2:
+        return 1
+    else:
+        return 0
+
+if __name__ == "__main__":
+    n = int(input("Digite o numero: "))
+    if primo(n):
+        print("Primo")
+    else:
+        print("Não primo")
+
